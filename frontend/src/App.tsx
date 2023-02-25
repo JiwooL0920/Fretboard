2f02fe4ec072514248fd79bfeeed9e72f4f70888
JiwooL0920 <JiwooL0920@gmail.com>
2023-02-25 00:05:09 -0500

    cleaned up frontend file structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import './App.css';
+import './css/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Fretboard 
-      </header>
-    </div>
+    <h1>Hello</h1>
   );
 }
 
