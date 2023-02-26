5d7db6b5f6c0b7dd37ca747f2b620875bce99d0d
JiwooL0920 <JiwooL0920@gmail.com>
2023-02-26 01:16:24 -0500

    tweaks to fretboard css design

--- frontend/src/components/fretboard/Fretboard.tsx
+++ frontend/src/components/fretboard/Fretboard.tsx
@@ -52,7 +52,7 @@ const String = (props: StringProps) => {
             string-number={stringNumber}
             string-note={stringNote}
         >
-            { [0,1,2,3,4,5,6,7,8,9,10,11,12].map( (i:number) => {
+            { [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15].map( (i:number) => {
                 const fretProps:FretProps = {
                     stringNumber: stringNumber,
                     fretNumber:i, 
