7d452666472c5ce38ddd45d788b76cd060652f9e
JiwooL0920 <JiwooL0920@gmail.com>
2023-04-04 23:52:49 -0400

    fix minor typo in routes.tsx

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,4 +1,4 @@
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/Homepage";
 import FretGamePage from "./pages/FretGamePage";
 import ScalePage from "./pages/ScalePage";
 import TriadPage from "./pages/TriadPage";
